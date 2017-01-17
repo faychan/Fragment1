ea7943795636b9f95d9b5e519a4288ffc4b8e276
Faychan <nor.iffat32@gmail.com>
2017-01-17 11:17:09 +0700

    -Fragment Independent View and Process

--- app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/InputFragment.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/InputFragment.java
@@ -1,29 +1,62 @@
 package id.sch.smktelkom_mlg.learn.fragment1;
 
-
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class InputFragment extends Fragment {
-
-
+    EditText etName;
+    EditText etUmur;
+    IListener mListener;
     public InputFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false);
+        View view = inflater.inflate(R.layout.fragment_input, container, false);
+
+        etName = (EditText) view.findViewById(R.id.editTextName);
+        etUmur = (EditText) view.findViewById(R.id.editTextUmur);
+
+        view.findViewById(R.id.bProses)
+                .setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        doProses();
+                    }
+                });
+        return view;
     }
 
+    private void doProses(){
+        if(mListener != null){
+            mListener.doProses(etName.getText().toString(),
+                    etUmur.getText().toString());
+        }
+    }
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(context instanceof InputFragment.IListener){
+            mListener = (InputFragment.IListener) context;
+        } else{
+            throw new RuntimeException(context.toString()
+                    + " must implement InputFragment.IListener");
+        }
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface IListener{
+        void doProses(String name, String age);
+    }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/fragment1/MainActivity.java
@@ -2,36 +2,21 @@ package id.sch.smktelkom_mlg.learn.fragment1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    EditText etName;
-    EditText etUmur;
-    TextView tvHasil;
+public class MainActivity extends AppCompatActivity
+        implements InputFragment.IListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        etName = (EditText) findViewById(R.id.editTextName);
-        etUmur = (EditText) findViewById(R.id.editTextUmur);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
-
-        findViewById(R.id.bProses)
-                .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doProcess();
-            }
-        });
     }
 
-    private void doProcess() {
-        tvHasil.setText(etName.getText().toString()
-                +"("+etUmur.getText().toString()+" tahun)");
+    @Override
+    public void doProses(String name, String age) {
+        OutputFragment fragOut =
+                (OutputFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragmentOutput);
+        fragOut.doProses(name, age);
     }
 }
